b5d5dc49cd959b2c500a0cc180e107d66fbde868
wvanvian <146589485+wvanvian@users.noreply.github.com>
2023-11-20 02:35:50 +0000

    modal working

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
 
-  delete "logout", to: "session#destroy"
+  get "logout", to: "session#destroy"
 
   get "sign_in", to: "session#new"
   post "sign_in", to: "session#create"
