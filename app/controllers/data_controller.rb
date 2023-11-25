8f87ed7a04a28e47a56e307b7f0e6bee80de8267
wvanvian <146589485+wvanvian@users.noreply.github.com>
2023-11-25 03:17:25 +0000

    disply on the page

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,7 +1,9 @@
 class DataController < ApplicationController
   before_action :required_user_logged_in!
   skip_forgery_protection
+  
   require 'csv'
+  require 'gruff'
 
   FILE_EXT = [".csv"]
 
@@ -37,61 +39,8 @@ class DataController < ApplicationController
   def visualize
     @filename = params[:param_name]
     path = Rails.root.join("public/uploads/files/", @filename)
-    @dataTable = CSV.parse(File.read("#{path}"), headers: true)
-    @dataTable.by_col!
-
-
-    #row,record,x_localized,y_localized,x_bin,y_bin,impactor,frequency,thickness,expected_thickness,amplitude,noise_amplitude,noise_ratio,voltage_max,
-    #thickness_frequency_amplitude,p_wave_integral,transducers_clipped,transducers_amp_max,exp_decay,time_delay,contact_time,starting_frequency,ending_frequency,
-    #frequency_range,calculated_cpp,ie_defects,pre_impact_resonance_frequency,pre_impact_resonance_frequency_amplitude,frequencies,mean_amplitudes,impactor_mass,
-    #sensor_ids,noise,clipped,debond,thickness_diff,defect,ie_defect
-
-    g = Gruff::Line.new
-    g.title = "A Line Graph"
-    g.data 'Fries', [20, 23, 19, 8]
-    g.data 'Hamburgers', [50, 19, 99, 29]
-    g.write("line.png")
-
-    require 'gruff'
-
-
-    data = [
-      [1, 1, 5],
-      [1, 2, 7],
-      [2, 1, 4],
-      [2, 2, 6]
-    ]
-
-    # Separate the data into x, y, and value arrays
-    x_data_points = data.map { |point| point[0] }
-    y_data_points = data.map { |point| point[1] }
-    values = data.map { |point| point[2] }
-
-    # Create a Gruff::Scatter plot
-    heatmap = Gruff::Scatter.new
-    heatmap.title = 'Heatmap'
-
-    #g = Gruff::Scatter.new(800)
-    heatmap.data :apples, [1,2,3,4], [4,3,2,1]
-    heatmap.data 'oranges', [5,7,8], [4,1,7]
-    heatmap.theme = {
-      colors: [
-        '#a9dada', # blue
-        '#aedaa9', # green
-        '#daaea9', # peach
-        '#dadaa9', # yellow
-        '#a9a9da', # dk purple
-        '#daaeda', # purple
-        '#dadada' # grey
-      ],
-      marker_color: '#aea9a9', # Grey
-      font_color: 'black',
-      background_colors: 'white'
-    }.freeze
-    heatmap.write('scatter.png')
-
-
-    csv_data = @dataTable
+    csv_data = CSV.parse(File.read("#{path}"), headers: true)
+    csv_data.by_col!
 
     # Extract data from CSV columns
     x_data = csv_data['x'].map(&:to_f)
@@ -115,10 +64,7 @@ class DataController < ApplicationController
     above_twentyfour_x = Array.new
     above_twentyfour_y = Array.new
 
-
-    pp('-------------------------------------')
     thickness_data.each_index do |i|
-      pp(thickness_data.at(i).to_f < 15, i)
       if thickness_data.at(i).to_f >= 24
         above_twentyfour_x.push(x_data.at(i)) 
         above_twentyfour_y.push(y_data.at(i))
@@ -154,25 +100,22 @@ class DataController < ApplicationController
       end
 
     end
-    pp(y_data)
-    pp('------------------------------------')
 
     # Create a Gruff::Scatter plot
-    heatmap = Gruff::Scatter.new
-    heatmap.title = 'Impact Echo Thickness'
-    heatmap.minimum_x_value = x_data.min - 0.05
-    heatmap.maximum_x_value = x_data.max + 0.05
+    heatmap = Gruff::Scatter.new(800)
 
-    #g = Gruff::Scatter.new(800)
     heatmap.data "< 6in", below_six_x, below_six_y
-    heatmap.data '>= 6in & < 9in', six_nine_x, six_nine_y
-    heatmap.data '>= 9in & < 12in', nine_twelve_x, nine_twelve_y
-    heatmap.data '>= 12in & < 15in', twelve_fifteen_x, twelve_fifteen_y
-    heatmap.data '>= 15in & < 18in', fifteen_eighteen_x, fifteen_eighteen_y
-    heatmap.data '>= 18in & < 21in', eighteen_twentyone_x, eighteen_twentyone_y
-    heatmap.data '>= 21in & < 24in', twentyone_twentyfour_x, twentyone_twentyfour_y
+    heatmap.data '< 9in', six_nine_x, six_nine_y
+    heatmap.data '< 12in', nine_twelve_x, nine_twelve_y
+    heatmap.data '< 15in', twelve_fifteen_x, twelve_fifteen_y
+    heatmap.data '< 18in', fifteen_eighteen_x, fifteen_eighteen_y
+    heatmap.data '< 21in', eighteen_twentyone_x, eighteen_twentyone_y
+    heatmap.data '< 24in', twentyone_twentyfour_x, twentyone_twentyfour_y
     heatmap.data '>= 24in', above_twentyfour_x, above_twentyfour_y
 
+
+    heatmap.minimum_x_value = x_data.min - 0.05
+    heatmap.maximum_x_value = x_data.max + 0.05
     heatmap.theme = {
       colors: [
         "#007BFF",  # Electric Blue
@@ -185,24 +128,16 @@ class DataController < ApplicationController
         "#00A8E8"   # Deep Sky Blue
       ],
       marker_color: '#aea9a9', # Grey
-      #marker_font_size: '20px',
-      font_color: 'white',
-      background_colors: 'white'
+      font_color: '#F0F0F0',
+      font: '9px',
+      background_colors: 'black'
     }.freeze
-
-    #heatmap.data('Data Series').marker_scale = 2.0
     heatmap.circle_radius = 5
     heatmap.legend_font_size = 9
     heatmap.legend_at_bottom = true
-    heatmap.write('scatter1.png')
+    heatmap.marker_font_size = 9
 
-    pp(x_data.max)
-    pp(y_data.max)
-
-
-
-
-
+    heatmap.write('app/assets/stylesheets/thickness.png')
 
   end
 
