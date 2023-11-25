c5702b987a8b0b917e1df14ef19c3de231a4d143
wvanvian <146589485+wvanvian@users.noreply.github.com>
2023-11-25 04:10:30 +0000

    both plots working

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -46,6 +46,11 @@ class DataController < ApplicationController
     x_data = csv_data['x'].map(&:to_f)
     y_data = csv_data['y'].map(&:to_f)
     thickness_data = csv_data['thickness'].map(&:to_f)
+    amplitude_data = csv_data['amplitude'].map(&:to_f)
+    
+    amplitude_max = amplitude_data.max
+    x_min = x_data.min
+    x_max = x_data.max
 
     below_six_x = Array.new
     below_six_y = Array.new
@@ -64,32 +69,37 @@ class DataController < ApplicationController
     above_twentyfour_x = Array.new
     above_twentyfour_y = Array.new
 
+    low_x = Array.new
+    low_y = Array.new
+    high_x = Array.new
+    high_y = Array.new
+
     thickness_data.each_index do |i|
-      if thickness_data.at(i).to_f >= 24
+      if thickness_data.at(i) >= 24
         above_twentyfour_x.push(x_data.at(i)) 
         above_twentyfour_y.push(y_data.at(i))
 
-      elsif thickness_data.at(i).to_f >= 21
+      elsif thickness_data.at(i) >= 21
         twentyone_twentyfour_x.push(x_data.at(i)) 
         twentyone_twentyfour_y.push(y_data.at(i))
 
-      elsif thickness_data.at(i).to_f >= 18
+      elsif thickness_data.at(i) >= 18
         eighteen_twentyone_x.push(x_data.at(i)) 
         eighteen_twentyone_y.push(y_data.at(i))
 
-      elsif thickness_data.at(i).to_f >= 15
+      elsif thickness_data.at(i) >= 15
         fifteen_eighteen_x.push(x_data.at(i)) 
         fifteen_eighteen_y.push(y_data.at(i))
 
-      elsif thickness_data.at(i).to_f  >= 12
+      elsif thickness_data.at(i) >= 12
         twelve_fifteen_x.push(x_data.at(i)) 
         twelve_fifteen_y.push(y_data.at(i)) 
 
-      elsif thickness_data.at(i).to_f  >= 9
+      elsif thickness_data.at(i) >= 9
         nine_twelve_x.push(x_data.at(i)) 
         nine_twelve_y.push(y_data.at(i)) 
         
-      elsif thickness_data.at(i).to_f >= 6
+      elsif thickness_data.at(i) >= 6
         six_nine_x.push(x_data.at(i)) 
         six_nine_y.push(y_data.at(i)) 
 
@@ -99,24 +109,39 @@ class DataController < ApplicationController
 
       end
 
+      if amplitude_data.at(i) < amplitude_max * 0.5
+        low_x.push(x_data.at(i))
+        low_y.push(y_data.at(i))
+      else
+        high_x.push(x_data.at(i))
+        high_y.push(y_data.at(i))
+      end
+
     end
 
+    get_thickness_heatmap(below_six_x, below_six_y, six_nine_x, six_nine_y, nine_twelve_x, nine_twelve_y, twelve_fifteen_x, twelve_fifteen_y, fifteen_eighteen_x, fifteen_eighteen_y, eighteen_twentyone_x, eighteen_twentyone_y, twentyone_twentyfour_x, twentyone_twentyfour_y, above_twentyfour_x, above_twentyfour_y, x_min, x_max)
+    get_amplitude_heatmap(high_x, high_y, low_x, low_y, x_max, x_min)
+
+
+  end
+
+  def get_thickness_heatmap(below_six_x, below_six_y, six_nine_x, six_nine_y, nine_twelve_x, nine_twelve_y, twelve_fifteen_x, twelve_fifteen_y, fifteen_eighteen_x, fifteen_eighteen_y, eighteen_twentyone_x, eighteen_twentyone_y, twentyone_twentyfour_x, twentyone_twentyfour_y, above_twentyfour_x, above_twentyfour_y, x_min, x_max)
     # Create a Gruff::Scatter plot
-    heatmap = Gruff::Scatter.new(800)
+    thickness_heatmap = Gruff::Scatter.new(800)
 
-    heatmap.data "< 6in", below_six_x, below_six_y
-    heatmap.data '< 9in', six_nine_x, six_nine_y
-    heatmap.data '< 12in', nine_twelve_x, nine_twelve_y
-    heatmap.data '< 15in', twelve_fifteen_x, twelve_fifteen_y
-    heatmap.data '< 18in', fifteen_eighteen_x, fifteen_eighteen_y
-    heatmap.data '< 21in', eighteen_twentyone_x, eighteen_twentyone_y
-    heatmap.data '< 24in', twentyone_twentyfour_x, twentyone_twentyfour_y
-    heatmap.data '>= 24in', above_twentyfour_x, above_twentyfour_y
+    thickness_heatmap.data "< 6in", below_six_x, below_six_y
+    thickness_heatmap.data '< 9in', six_nine_x, six_nine_y
+    thickness_heatmap.data '< 12in', nine_twelve_x, nine_twelve_y
+    thickness_heatmap.data '< 15in', twelve_fifteen_x, twelve_fifteen_y
+    thickness_heatmap.data '< 18in', fifteen_eighteen_x, fifteen_eighteen_y
+    thickness_heatmap.data '< 21in', eighteen_twentyone_x, eighteen_twentyone_y
+    thickness_heatmap.data '< 24in', twentyone_twentyfour_x, twentyone_twentyfour_y
+    thickness_heatmap.data '>= 24in', above_twentyfour_x, above_twentyfour_y
 
 
-    heatmap.minimum_x_value = x_data.min - 0.05
-    heatmap.maximum_x_value = x_data.max + 0.05
-    heatmap.theme = {
+    thickness_heatmap.minimum_x_value = x_min - 0.05
+    thickness_heatmap.maximum_x_value = x_max + 0.05
+    thickness_heatmap.theme = {
       colors: [
         "#007BFF",  # Electric Blue
         "#00C853",  # Emerald Green
@@ -127,18 +152,44 @@ class DataController < ApplicationController
         "#FF2E63",  # Magenta
         "#00A8E8"   # Deep Sky Blue
       ],
-      marker_color: '#aea9a9', # Grey
+      marker_color: 'black',
       font_color: '#F0F0F0',
       font: '9px',
       background_colors: 'black'
     }.freeze
-    heatmap.circle_radius = 5
-    heatmap.legend_font_size = 9
-    heatmap.legend_at_bottom = true
-    heatmap.marker_font_size = 9
+    thickness_heatmap.circle_radius = 5
+    thickness_heatmap.legend_font_size = 9
+    thickness_heatmap.legend_at_bottom = true
+    thickness_heatmap.marker_font_size = 9
 
-    heatmap.write('app/assets/stylesheets/thickness.png')
+    thickness_heatmap.write('app/assets/stylesheets/thickness.png')
+  end
 
+
+  def get_amplitude_heatmap(high_x, high_y, low_x, low_y, x_max, x_min)
+    amplitude_heatmap = Gruff::Scatter.new(800)
+
+    amplitude_heatmap.data "< 1M Mv", high_x, high_y
+    amplitude_heatmap.data '> 1M Mv', low_x, low_y
+
+    amplitude_heatmap.minimum_x_value = x_min - 0.05
+    amplitude_heatmap.maximum_x_value = x_max + 0.05
+    amplitude_heatmap.theme = {
+      colors: [
+        "#FF0000",  # Red
+        "#C0C0C0"   # Gray
+      ],
+      marker_color: 'black', 
+      font_color: '#F0F0F0',
+      font: '9px',
+      background_colors: 'black'
+    }.freeze
+    amplitude_heatmap.circle_radius = 5
+    amplitude_heatmap.legend_font_size = 9
+    amplitude_heatmap.legend_at_bottom = true
+    amplitude_heatmap.marker_font_size = 9
+
+    amplitude_heatmap.write('app/assets/stylesheets/amplitude.png')
   end
 
     
